6dc4196c1024e101a1736910bbffc85cf2723fcc
alibeep1 <ali_90082@aucegypt.edu>
2021-02-02 12:24:38 +0200

    solveNQUtil

---
+++ solveNQUtil.cpp
@@ -0,0 +1,37 @@
+#include "isSafe.h"
+
+using namespace std;
+
+/* A recursive utility function to solve N 
+   Queen problem */
+bool solveNQUtil(int board[N][N], int col) 
+{ 
+    /* base case: If all queens are placed 
+      then return true */
+    if (col >= N) 
+        return true; 
+  
+    /* Consider this column and try placing 
+       this queen in all rows one by one */
+    for (int i = 0; i < N; i++) { 
+        /* Check if the queen can be placed on 
+          board[i][col] */
+        if (isSafe(board, i, col)) { 
+            /* Place this queen in board[i][col] */
+            board[i][col] = 1; 
+  
+            /* recur to place rest of the queens */
+            if (solveNQUtil(board, col + 1)) 
+                return true; 
+  
+            /* If placing queen in board[i][col] 
+               doesn't lead to a solution, then 
+               remove queen from board[i][col] */
+            board[i][col] = 0; // BACKTRACK 
+        } 
+    } 
+  
+    /* If the queen cannot be placed in any row in 
+        this colum col  then return false */
+    return false; 
+} 
---
+++ solveNQUtil.h
@@ -0,0 +1,7 @@
+#define N 4 
+#include <stdbool.h> 
+#include <stdio.h>
+
+using namespace std;
+
+bool solveNQUtil(int board[N][N], int col);
